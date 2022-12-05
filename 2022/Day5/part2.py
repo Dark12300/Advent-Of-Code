34b85af70a2c27a45d778d807e5ff76d60369e66
Dark12300 <dark1232478@gmail.com>
2022-12-05 22:36:15 +0000

    solved day 5

---
+++ 2022/Day5/part2.py
@@ -0,0 +1,33 @@
+with open("input.txt", "r") as file:
+    crates, moves = file.read().split("\n\n")
+    crate_lines = crates.splitlines()
+    move_lines = moves.splitlines()
+    
+supply_stacks = [[] for x in range(0, max(map(int, crate_lines[-1].split())))]
+current_stack = -1
+for line in crate_lines[:-1]:
+    for x in range(0, len(line)):
+        character = line[x]
+        
+        if (x + 3) % 4 == 0:
+            current_stack = current_stack + 1
+
+        if x == len(line) - 1:
+            current_stack = -1
+            
+        if character.isalpha():
+            supply_stacks[current_stack].insert(0, character)
+
+for line in move_lines:
+    moves_list = line.split()
+    moves_list = [int(moves_list[1]), int(moves_list[3]), int(moves_list[5])]
+
+    original_supply_stack = supply_stacks[moves_list[1]-1]
+    destination_supply_stack = supply_stacks[moves_list[2]-1]
+    to_transfer = original_supply_stack[len(original_supply_stack)-moves_list[0]:]
+
+    supply_stacks[moves_list[2]-1] = destination_supply_stack + to_transfer
+    supply_stacks[moves_list[1]-1] = original_supply_stack[:len(original_supply_stack)-moves_list[0]]
+
+for stack in supply_stacks:
+    print(stack[-1])
