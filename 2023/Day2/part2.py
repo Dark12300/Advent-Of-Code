9a22b35bdba335b46bbfe94e348e96cadfc0b0c5
Dark12300 <dark1232478@gmail.com>
2023-12-06 20:18:29 +0000

    solved day 2

---
+++ 2023/Day2/part2.py
@@ -0,0 +1,26 @@
+with open("input.txt", "r") as file:
+    lines = file.read().splitlines()
+
+def parse_line(line):
+    game_info, information = line.split(":")
+    subsets_id = game_info.split(" ")[1]
+    subsets = information.split(";")
+
+    subsets_info = {"red": 0, "green": 0, "blue": 0}   #r, g, b max values out of all subsets
+    for subset in subsets:
+        pairs = subset.split(",")
+        for pair in pairs:
+            count, colour = pair.strip().split(" ")
+
+            if subsets_info[colour] < int(count):
+                subsets_info[colour] = int(count)
+
+    return int(subsets_id), subsets_info
+
+total = 0
+for line in lines:
+    subsets_id, subsets_info = parse_line(line)
+    power = subsets_info["red"] * subsets_info["green"] * subsets_info["blue"]
+    total += power
+
+print(total)
